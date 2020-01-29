770679b9dac5bc6e2f74628a10c1b4b4752f0d92
VISHALBHARDWAJ123 <60150170+VISHALBHARDWAJ123@users.noreply.github.com>
2020-01-29 10:26:38 +0530

    Add files via upload

---
+++ t.php
@@ -0,0 +1,22 @@
+<html>
+<body>
+<?php
+$servername = "localhost";
+$username = "tamanna";
+$password = "tamanna123";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";<br>
+$sql = "CREATE DATABASE myDB";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
+?>
